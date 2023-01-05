7d533d15e835939ff110d7b7f8db6c46e54acbd3
xiyu1984 <shanellyy@126.com>
2023-01-05 12:26:48 +0800

    create pallet o-amm simu

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -49,6 +49,7 @@ pub use sp_runtime::{Perbill, Permill};
 
 /// Import the template pallet.
 pub use pallet_template;
+pub use pallet_o_amm_simu;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -280,6 +281,10 @@ impl pallet_template::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 }
 
+impl pallet_o_amm_simu::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub struct Runtime
@@ -298,6 +303,7 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		OAMMSModule: pallet_o_amm_simu,
 	}
 );
 
@@ -345,6 +351,7 @@ mod benches {
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
 		[pallet_template, TemplateModule]
+		[pallet_o_amm_simu, OAMMSModule]
 	);
 }
 
